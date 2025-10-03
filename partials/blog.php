4d5153843147e0407a06d7a923b3d6ac1621bb09
Mubaraq-Ay <ayanlekemubaraq@gmail.com>
2025-10-03 21:50:43 +0100

    md

--- partials/blog.php
+++ partials/blog.php
@@ -3,18 +3,18 @@
 
 
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <section id="portfolio" class="portfolio section">
 
     <!-- Section Title -->
     <div class="container text-center" data-aos="fade-up">
-        
-            <a href="./blog_index.php">
-                <h2>My Blogs</h2>
-                 
-            </a>
-        
+
+        <a href="./blog_index.php">
+            <h2>My Blogs</h2>
+
+        </a>
+
     </div><!-- End Section Title -->
 
 
